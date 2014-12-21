d9244f5396c8556f677aa8e0bd8ebdab6f796aba
Rachel Prince <rachel.prince@yale.edu>
2014-12-21 13:16:35 -0800

    Error styling for page

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ group :production do
   gem 'rails_12factor', '0.0.2'
   gem 'thin'
 end
-gem'bcrypt-ruby', '~> 3.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
 # Use sqlite3 as the database for Active Record
--- app/models/traineeship.rb
+++ app/models/traineeship.rb
@@ -1,18 +1,17 @@
 class Traineeship < ActiveRecord::Base
-	validates :ep_name,  presence: true
+	validates_presence_of :ep_name, :message => "Please enter your name.", on: :create
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :ep_email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX }
- validates :manager_email, allow_nil: true,
- 					 format: { with: VALID_EMAIL_REGEX }  
-    validates :region,  presence: true
-    validates :country,  presence: true   
-    validates :lc, presence: true
-    validates :tn_type, presence: true          
-    validates :rate_lc,  presence: true, numericality: true
-    validates :rate_tn, presence: true, numericality: true
-    validates :lc_comments, presence: true
-   	validates :tn_comments, presence: true
-   	validates :rate_homelc, presence: true
-   	validates :homelc_comments, presence: true 
+  validates_presence_of :ep_email, message: "Please enter your email.", on: :create
+  validates_format_of :ep_email,  with: VALID_EMAIL_REGEX, message: "Please enter a valid email.", on: :create
+ validates_format_of :manager_email, with:  VALID_EMAIL_REGEX,  allow_nil: true, message: "Please enter a valid email for your manager or leave it blank.", on: :create
+    validates_presence_of :region, message: "Please enter a region for your TN."
+    validates_presence_of :country, message: "Please enter a country for your TN."
+    validates_presence_of :lc, message: "Please enter an LC for your TN."
+    validates_presence_of :tn_type, message: "Please enter GIP or GCDP for your TN."         
+    validates_presence_of :rate_lc, message: "Please enter a number 1 through 10 for your host LC rating.", numericality: true
+    validates_presence_of :rate_tn, message: "Please enter a number 1 through 10 for your TN rating.", numericality: true
+    validates_presence_of :lc_comments,  message: "Please provide comments about your host LC.", on: :create
+   	validates_presence_of :tn_comments, message: "Please provide comments about your TN.", on: :create
+   	validates_presence_of :rate_homelc, message: "Please enter a number 1 through 10 for your home LC rating.", numericality: true
+   	validates_presence_of :homelc_comments, message: "Please provide comments about your home LC", on: :create
 end
