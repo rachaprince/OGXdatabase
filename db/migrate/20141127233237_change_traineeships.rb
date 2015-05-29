2dea7ebd15768ad40e922f14d60bb7bf90367ad7
Rachel Prince <rachel.prince@yale.edu>
2015-05-28 20:49:29 -0700

    heroku bugs

--- db/migrate/20141127233237_change_traineeships.rb
+++ db/migrate/20141127233237_change_traineeships.rb
@@ -1,8 +1,6 @@
 class ChangeTraineeships < ActiveRecord::Migration
   def change
   	
-  	remove_column :traineeships, :country_id
-  	remove_column :traineeships, :lc_id
-  	remove_column :traineeships, :region_id
+  	
   end
 end
